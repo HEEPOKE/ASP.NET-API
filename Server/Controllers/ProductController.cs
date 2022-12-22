18777ff15b92a91cb44c55c0dbbdff99be73a321
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-22 15:06:12 +0700

    Update ProductController.cs

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Server.Interfaces;
+using Server.Services;
 
 namespace Server.Controllers
 {
@@ -11,11 +13,10 @@ namespace Server.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetAllProduct()
         {
-            return await _ProductService.GetAllProduct();
+            return await ProductService.GetAllProduct();
         }
     }
 }
