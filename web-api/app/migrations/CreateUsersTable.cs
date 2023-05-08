7ff813c82d6d59b943689ab23f6c4a3f567fbf8a
HEEPOKE <Damon1fx@gmail.com>
2023-05-08 12:10:36 +0700

    :money_with_wings: update

---
+++ web-api/app/migrations/CreateUsersTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using app.Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace app.Migrations
+{
+    public partial class AddUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Tel = table.Column<string>(nullable: true),
+                    Role = table.Column<int>(nullable: false, defaultValue: (int)UserRole.User),
+                    CreatedAt = table.Column<DateTime>(nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedAt = table.Column<DateTime>(nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
